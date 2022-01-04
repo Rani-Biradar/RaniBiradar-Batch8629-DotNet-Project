bbf449ce2905dce347307f737d461c271e4f62dd
Rani-Biradar <95352807+Rani-Biradar@users.noreply.github.com>
2022-01-04 16:43:37 +0530

    Add files via upload

---
+++ library system management/issue_books.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace Libray_Management_Project
+{
+    partial class issue_books
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_enrollment = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_studentname = new System.Windows.Forms.TextBox();
+            this.txt_studentdepartment = new System.Windows.Forms.TextBox();
+            this.txt_studentsem = new System.Windows.Forms.TextBox();
+            this.txt_studentcontact = new System.Windows.Forms.TextBox();
+            this.txt_studentemail = new System.Windows.Forms.TextBox();
+            this.txt_booksname = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.txt_booksname);
+            this.panel1.Controls.Add(this.txt_studentemail);
+            this.panel1.Controls.Add(this.txt_studentcontact);
+            this.panel1.Controls.Add(this.txt_studentsem);
+            this.panel1.Controls.Add(this.txt_studentdepartment);
+            this.panel1.Controls.Add(this.txt_studentname);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txt_enrollment);
+            this.panel1.Location = new System.Drawing.Point(33, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(845, 463);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txt_enrollment
+            // 
+            this.txt_enrollment.Location = new System.Drawing.Point(24, 69);
+            this.txt_enrollment.Name = "txt_enrollment";
+            this.txt_enrollment.Size = new System.Drawing.Size(135, 20);
+            this.txt_enrollment.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search Student";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Enrollment No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(367, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(340, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Student Department";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(374, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Student Sem";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(358, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Student Contact";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(370, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Student Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(374, 334);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Books Name";
+            // 
+            // txt_studentname
+            // 
+            this.txt_studentname.Location = new System.Drawing.Point(470, 24);
+            this.txt_studentname.Name = "txt_studentname";
+            this.txt_studentname.Size = new System.Drawing.Size(145, 20);
+            this.txt_studentname.TabIndex = 9;
+            // 
+            // txt_studentdepartment
+            // 
+            this.txt_studentdepartment.Location = new System.Drawing.Point(470, 76);
+            this.txt_studentdepartment.Name = "txt_studentdepartment";
+            this.txt_studentdepartment.Size = new System.Drawing.Size(145, 20);
+            this.txt_studentdepartment.TabIndex = 10;
+            // 
+            // txt_studentsem
+            // 
+            this.txt_studentsem.Location = new System.Drawing.Point(470, 122);
+            this.txt_studentsem.Name = "txt_studentsem";
+            this.txt_studentsem.Size = new System.Drawing.Size(145, 20);
+            this.txt_studentsem.TabIndex = 11;
+            // 
+            // txt_studentcontact
+            // 
+            this.txt_studentcontact.Location = new System.Drawing.Point(470, 176);
+            this.txt_studentcontact.Name = "txt_studentcontact";
+            this.txt_studentcontact.Size = new System.Drawing.Size(145, 20);
+            this.txt_studentcontact.TabIndex = 12;
+            // 
+            // txt_studentemail
+            // 
+            this.txt_studentemail.Location = new System.Drawing.Point(470, 226);
+            this.txt_studentemail.Name = "txt_studentemail";
+            this.txt_studentemail.Size = new System.Drawing.Size(145, 20);
+            this.txt_studentemail.TabIndex = 13;
+            // 
+            // txt_booksname
+            // 
+            this.txt_booksname.Location = new System.Drawing.Point(470, 331);
+            this.txt_booksname.Name = "txt_booksname";
+            this.txt_booksname.Size = new System.Drawing.Size(145, 20);
+            this.txt_booksname.TabIndex = 14;
+            this.txt_booksname.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_booksname_KeyDown);
+            this.txt_booksname.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txt_booksname_KeyUp);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(361, 380);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 15;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(470, 276);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(145, 20);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(351, 282);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Books Issue Date";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(192, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Issue Books";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(470, 357);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(145, 95);
+            this.listBox1.TabIndex = 19;
+            this.listBox1.Visible = false;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
+            // issue_books
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 520);
+            this.Controls.Add(this.panel1);
+            this.Name = "issue_books";
+            this.Text = "issue_books";
+            this.Load += new System.EventHandler(this.issue_books_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_booksname;
+        private System.Windows.Forms.TextBox txt_studentemail;
+        private System.Windows.Forms.TextBox txt_studentcontact;
+        private System.Windows.Forms.TextBox txt_studentsem;
+        private System.Windows.Forms.TextBox txt_studentdepartment;
+        private System.Windows.Forms.TextBox txt_studentname;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_enrollment;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ library system management/view_student_info.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Libray_Management_Project
+{
+    public partial class view_student_info : Form
+    {
+        SqlConnection con = new SqlConnection(@" Data Source=DESKTOP-UFP98S6\SQLEXPRESS;Initial Catalog=Library_Management_System;Integrated Security=True;Pooling=False");
+
+        public view_student_info()
+        {
+            InitializeComponent();
+        }
+
+        private void view_student_info_Load(object sender, EventArgs e)
+        {
+
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            fill_grid();
+
+           
+        }
+        public void fill_grid()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from student_info";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                dataGridView1.Columns.Clear();
+                dataGridView1.Refresh();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from student_info where Student_Name like ('%"+textBox1.Text +"%')";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = Convert.ToInt32(dataGridView1.SelectedCells[0].Value.ToString());
+            SqlCommand cmd = con. CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from student_info where id = "+i+"";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            foreach(DataRow dr in dt.Rows)
+            {
+                studentname.Text = dr["Student_Name"].ToString();
+                enrollmentno.Text = dr["Student_Enrollment_No"].ToString();
+                department.Text = dr["Student_Department"].ToString();
+                sem.Text = dr["Student_Sem"].ToString();
+                contact.Text = dr["Student_Contact"].ToString();
+                email.Text = dr["Student_Email"].ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i;
+            i = Convert.ToInt32(dataGridView1.SelectedCells[0].Value.ToString());
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update student_info set Student_Name='"+studentname .Text +"',Student_Enrollment_No='"+enrollmentno.Text +"',Student_Department='"+department .Text +"',Student_Sem='"+sem .Text +"',Student_Contact='"+contact .Text +"',Student_Email='"+email .Text +"' where id="+i+" ";
+            cmd.ExecuteNonQuery();
+            fill_grid();
+            MessageBox.Show("record updated succesfully");
+        }
+    }
+}
